aca8435dbef1457d4c5dcbe61167a9a7c72d50f7
eswarpavan664 <itonystark58@gmail.com>
2021-05-23 17:58:45 +0530

    about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,11 +6,53 @@ import '../about.css';
 class  About extends Component {
   render() {
     return (
-      <div>
-        
-         <h1>hello</h1>
-         
-      </div>
+        <div>
+
+
+          <section>
+                
+
+            <h1 style={{textAlign:'center',color:'blue',fontSize:25,marginTop:'5%',fontWeight:'bold'}}>ABOUT US</h1>
+            
+
+
+
+                <div className="textdiv">
+                    <div className="imagdiv">
+                    <img src={image1} className="images"/>
+                    <h2>Pavaneswar</h2>
+                    </div>
+
+                    <p>This website is developed by pavaneswar and santosh .HTML and CSS can be both used to display align and float images on your website. In this article, we’ll show you how to use HTML to align images to text (or other page elements) and how to use CSS to float images, wrapping text around it as you’d see in a newspaper or magazine. </p>
+                </div>
+
+               
+
+                
+
+
+
+          </section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
     );
   }
 }
