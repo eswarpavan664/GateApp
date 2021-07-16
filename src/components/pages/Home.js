3d6caad35a5a8bd4fb0ce6016f3c54308087d6d0
eswarpavan664 <itonystark58@gmail.com>
2021-07-16 13:59:27 +0530

    version 8.0

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -35,36 +35,7 @@ function Home() {
           
           <section style={{backgroundColor:'#5DADE2',paddingTop:'10%',paddingBottom:'20%'  }}>     
      
-     <div id="carouselExampleIndicators" class="carousel slide"  data-ride="carousel"  id="slider">
-
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-         
-          <div class="carousel-inner"  >
-            <div class="carousel-item active">
-              <img class="d-block w-100" src="https://english.cdn.zeenews.com/sites/default/files/styles/zm_700x400/public/2021/01/09/909596-gate-exam-2021-admit.jpg" alt="First slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="https://d2cyt36b7wnvt9.cloudfront.net/exams/wp-content/uploads/2019/01/09171711/ISRO-Exam-Pattern.jpg" alt="Second slide"/>
-            </div>
-            <div class="carousel-item">
-              <img class="d-block w-100" src="https://www.exams88.in/wp-content/uploads/2018/01/IES-2021.jpg" alt="Third slide"/>
-            </div>
-          </div>
-
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-      </div>
+    
 
 
 
